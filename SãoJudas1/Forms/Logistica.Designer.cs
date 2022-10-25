f75b1e5ebfb55bc07a67237b4272f957dfb2889c
Higor Chagas <higornchagas@gmail.com>
2022-10-25 16:57:12 -0300

    Adicionando botoes da agenda

--- SãoJudas1/Forms/Logistica.Designer.cs
+++ SãoJudas1/Forms/Logistica.Designer.cs
@@ -76,10 +76,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(311, 257);
+            this.ControlBox = false;
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Logistica";
             this.Text = "Logistica";
             this.ResumeLayout(false);

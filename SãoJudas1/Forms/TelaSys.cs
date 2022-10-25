fe01ac7c14b1e0222a971cff898417be28801854
jrsaojudas <116647468+jrsaojudas@users.noreply.github.com>
2022-10-25 09:31:12 -0300

    Add files via upload

---
+++ SãoJudas1/Forms/TelaSys.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SãoJudas1.Forms
+{
+    public partial class TelaSys : Form
+    {        
+        public TelaSys()
+        {
+            InitializeComponent();
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void novoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "Todos os arquivos|*.*";            
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.Diagnostics.Process.Start(openFileDialog1.FileName);
+            }
+        }
+    }
+}

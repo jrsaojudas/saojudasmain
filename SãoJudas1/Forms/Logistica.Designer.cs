1559ae8a19ce2973281af4d69dd58ffd635bba71
Higor Chagas <higornchagas@gmail.com>
2022-10-25 16:28:35 -0300

    criano botoes da agenda

--- SãoJudas1/Forms/Logistica.Designer.cs
+++ SãoJudas1/Forms/Logistica.Designer.cs
@@ -29,25 +29,72 @@
         private void InitializeComponent()
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(298, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(82, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AGENDA";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(64, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 55);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(64, 139);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 55);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(64, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(177, 55);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Logistica
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(298, 277);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Logistica";
             this.Text = "Logistica";
+            this.Load += new System.EventHandler(this.Logistica_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,5 +103,9 @@
         #endregion
 
         private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
---
+++ SãoJudas1/sql/dbo.Procedure_CPF.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE dbo.buscaCliCPF
+	@CPF varchar(15)
+AS
+	SELECT cpf, nome, endereco, telefone
+	FROM clientes
+	WHERE CPF=@CPF
+
+RETURN 0
+

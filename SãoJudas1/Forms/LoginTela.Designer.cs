ff1e55f1e1acdd309d74b45d48a831565755f36a
JONATHAN BARRETO <tecnologiasj03@gmail.com>
2022-10-25 10:41:20 -0300

    tela login up

--- SãoJudas1/Forms/LoginTela.Designer.cs
+++ SãoJudas1/Forms/LoginTela.Designer.cs
@@ -105,7 +105,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(382, 293);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
